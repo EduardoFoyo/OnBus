12bc0d59af60819b007f80606d079343160e5281
LuisPabloRocha <35905098+LuisPabloRocha@users.noreply.github.com>
2019-12-07 00:12:24 -0600

    3Bien

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { AlertController, } from '@ionic/angular';
+import {Directive, ElementRef, Input, OnInit, OnDestroy} from 'angular2/core';
+import {Gesture} from 'ionic-angular/gestures/gesture';
+
+
 
 @Component({
   selector: 'app-home',
@@ -9,7 +14,8 @@ import { NavController } from '@ionic/angular';
 export class HomePage {
 
   constructor(
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    private alertCtrl: AlertController
   ) {}
 
 
@@ -17,4 +23,15 @@ export class HomePage {
     this.navCtrl.navigateForward('/prueba');
   }
 
+  async presentAlert(e) {
+    const alert = await this.alertCtrl.create({
+    message: 'Tu reporte fue enviado. Será atendido con urgencia.',
+    subHeader: 'REPORTE ENVIADO',
+    buttons: ['Aceptar']
+   });
+   await alert.present(); 
+  }
 }
+
+
+
